bad40bfd51777ce728bc46e719295b56738c31e9
raBeta <raul.bethencourt.pro@gmail.com>
2022-01-07 14:37:56 +0100

    :wrench: update lvim

--- dotfiles/lvim/config.lua
+++ dotfiles/lvim/config.lua
@@ -1,264 +1,244 @@
---[[ Notes: {{{1
+--[[
+lvim is the global options object
 
-Useful Links and TODO
-https://github.com/Kethku/neovide
-http://cheat.sh
-Buffer bar info: https://github.com/romgrk/barbar.nvim
+Linters should be
+filled in as strings with either
+a global executable or a path to
+an executable
+]]
+-- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+vim.opt.relativenumber = true
+vim.opt.fileencoding = 'utf-8'
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 
-TODO: LSP Java
-TODO: LSP Perl
-TODO: LSP JavaScript
-TODO: LSP React
-TODO: LSP PHP
-TODO: LSP Bash
-TODO: LSP Python
-}}}1 ]]
-
--- Settings {{{1
-
+-- general
+lvim.log.level = "warn"
 lvim.format_on_save = false
-lvim.auto_complete = true
-lvim.colorscheme = "gruvqueen"
-lvim.auto_close_tree = 0
-lvim.wrap_lines = false
-lvim.timeoutlen = 100
+lvim.colorscheme = "gruvbox-material"
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
-lvim.ignore_case = true
-lvim.smart_case = true
-lvim.termguicolors = true
+-- add your own keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+-- unmap a default keymapping
+-- lvim.keys.normal_mode["<C-Up>"] = ""
+-- edit a default keymapping
+-- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
+
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+-- local _, actions = pcall(require, "telescope.actions")
+-- lvim.builtin.telescope.defaults.mappings = {
+--   -- for input mode
+--   i = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--     ["<C-n>"] = actions.cycle_history_next,
+--     ["<C-p>"] = actions.cycle_history_prev,
+--   },
+--   -- for normal mode
+--   n = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--   },
+-- }
+
+-- Use which-key to add extra bindings with the leader-key prefix
+-- Additional Leader bindings for WhichKey and key mappings {{{1
+lvim.builtin.which_key.mappings["o"] = {
+  name = "Replace",
+  r = { "<cmd>lua require('spectre').open()<cr>", "Replace" },
+  f = { "<cmd>lua require('spectre').open_file_search()<cr>", "Replace Buffer" },
+  w = { "<cmd>lua require('spectre').open_visual({select_word=true})<cr>", "Replace Word" },
+}
+lvim.builtin.which_key.mappings["r"] = {
+  "<cmd>RnvimrToggle<CR>", "Ranger"
+}
+lvim.builtin.which_key.mappings["t"] = {
+  "<cmd>exe v:count1 . 'ToggleTerm'<CR>", "Terminal"
+}
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+-- lvim.builtin.which_key.mappings["t"] = {
+--   name = "+Trouble",
+--   r = { "<cmd>Trouble lsp_references<cr>", "References" },
+--   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+--   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
+--   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+--   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
+-- }
+
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
-lvim.vsnip_dir = os.getenv "HOME" .. "/.config/lvim/snippets/"
-lvim.builtin.treesitter.ensure_installed = "all"
+lvim.builtin.terminal.active = true
+lvim.builtin.terminal.direction = 'vertical'
+lvim.builtin.terminal.size = 80
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 0
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
--- FIX: Disable java for now.
--- lvim.builtin.jdtls.active = false
+-- generic LSP settings
 
---}}}
+-- ---@usage disable automatic installation of servers
+lvim.lsp.automatic_servers_installation = true
 
--- Dashboard {{{1
+-- ---@usage Select which servers should be configured manually. Requires `:LvimCacheRest` to take effect.
+-- See the full default list `:lua print(vim.inspect(lvim.lsp.override))`
+-- vim.list_extend(lvim.lsp.override, { "pyright" })
 
--- Several shorter ASCII art logos, so not too much space is taken up.
--- lvim.builtin.dashboard.custom_header = {
---  }
+-- ---@usage setup a server -- see: https://www.lunarvim.org/languages/#overriding-the-default-configuration
+-- local opts = {} -- check the lspconfig documentation for a list of all possible options
+-- require("lvim.lsp.manager").setup("pylsp", opts)
 
-lvim.builtin.dashboard.custom_section = {
-    a = {description = {"  New File           "}, command = "DashboardNewFile" },
-    b = {description = {"  Find File          "}, command = "Telescope find_files", },
-    c = {description = {"  Ranger             "}, command = "RnvimrToggle"},
-    d = {description = {"  Recently Used Files"}, command = "Telescope oldfiles", },
-    e = {description = {"  Find Word          "}, command = "Telescope live_grep", },
-    f = {description = {"  Marks              "}, command = "Telescope marks"},
-    g = {description = {"  Settings           "}, command = ":e ~/.config/lvim/config.lua", },
-    h = {description = {"  Git Status         "}, command = "Telescope git_status" },
-    -- f = {description = {"  Neovim Config Files"}, command = "Telescope find_files cwd=" .. CONFIG_PATH, },
-    -- h = {description = {"  File Browser       "}, command = "Telescope file_browser" },
-    -- i = {description = {"  Load Last Session  "}, command = "SessionLoad"},
+-- you can set a custom on_attach function that will be used for all the language servers
+-- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+-- you can overwrite the null_ls setup table (useful for setting the root_dir function)
+-- lvim.lsp.null_ls.setup = {
+--   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
+-- }
+-- or if you need something more advanced
+-- lvim.lsp.null_ls.setup.root_dir = function(fname)
+--   if vim.bo.filetype == "javascript" then
+--     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
+--       or require("lspconfig/util").path.dirname(fname)
+--   elseif vim.bo.filetype == "php" then
+--     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
+--   else
+--     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
+--   end
+-- end
+
+-- -- set a formatter, this will override the language server formatting capabilities (if it exists)
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { exe = "black", filetypes = { "python" } },
+  { exe = "rustfmt", filetypes = { "rust" } },
+--   { exe = "isort", filetypes = { "python" } },
+--   {
+--     exe = "prettier",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     args = { "--print-with", "100" },
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "typescript", "typescriptreact" },
+--   },
 }
 
---}}}
+-- -- set additional linters
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  { exe = "flake8", filetypes = { "python" } },
+--   {
+--     exe = "shellcheck",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     args = { "--severity", "warning" },
+--   },
+--   {
+--     exe = "codespell",
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "javascript", "python" },
+--   },
+}
 
--- Additional Plugins {{{1
+lvim.builtin.dashboard.custom_header = {
+  "  8 888888888o.            .8. `8.`888b           ,8'  8 8888          ,8.       ,8.",
+  "  8 8888    `88.          .888. `8.`888b         ,8'   8 8888         ,888.     ,888.",
+  "  8 8888     `88         :88888. `8.`888b       ,8'    8 8888        .`8888.   .`8888.",
+  "  8 8888     ,88        . `88888. `8.`888b     ,8'     8 8888       ,8.`8888. ,8.`8888.",
+  "  8 8888.   ,88'       .8. `88888. `8.`888b   ,8'      8 8888      ,8'8.`8888,8^8.`8888.",
+  "  8 888888888P'       .8`8. `88888. `8.`888b ,8'       8 8888     ,8' `8.`8888' `8.`8888.",
+  "  8 8888`8b          .8' `8. `88888. `8.`888b8'        8 8888    ,8'   `8.`88'   `8.`8888.",
+  "  8 8888 `8b.       .8'   `8. `88888. `8.`888'         8 8888   ,8'     `8.`'     `8.`8888.",
+  "  8 8888   `8b.    .888888888. `88888. `8.`8'          8 8888  ,8'       `8        `8.`8888.",
+  "  8 8888     `88. .8'       `8. `88888. `8.`           8 8888 ,8'         `         `8.`8888.",
+}
+lvim.builtin.dashboard.custom_footer = "https://raulbethencourt.com"
+
+-- Additional Plugins
 lvim.plugins = {
-    {'Murtaza-Udaipurwala/gruvqueen', 
-        config = function()
-            require("gruvqueen").setup({
-                config = {
-                    style = 'material'
-                }
-            })
+  {"mg979/vim-visual-multi"},
+  {"sainnhe/gruvbox-material"},
+  {"ellisonleao/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}},
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+      vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+    end,
+  },
+  {
+    "kevinhwang91/rnvimr",
+      cmd = "RnvimrToggle",
+      config = function()
+        vim.g.rnvimr_draw_border = 1
+        vim.g.rnvimr_pick_enable = 1
+        vim.g.rnvimr_bw_enable = 1
         end,
-    },
-    {"ellisonleao/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}},
-    -- Colorizer: #ff0000, Blue, #f0f
-    {
-        "norcalli/nvim-colorizer.lua",
-        config = function()
-            require("user.colorizer").config()
-        end,
-    },
-    -- Better motions
-    {
-        "phaazon/hop.nvim",
-        event = "BufRead",
-        config = function()
-            require("user.hop").config()
-        end,
-    },
-    -- Todo comments.
-    -- FIX:Something to describe.
-    -- FIXME: Something to describe.
-    -- BUG:Something to describe.
-    -- FIXIT: Something to describe.
-    -- ISSUE: Something to describe.
-    -- TODO:Something to describe.
-    -- HACK:Something to describe.
-    -- WARN:Something to describe.
-    -- WARNING:Something to describe.
-    -- XXX:Something to describe.
-    -- PERF:Something to describe.
-    -- OPTIM:Something to describe.
-    -- PERFORMANCE:Something to describe.
-    -- OPTIMIZE:Something to describe.
-    -- NOTE:Something to describe.
-    -- INFO:Something to describe.
-    -- TEST: Test something.
-    -- OK: Something.
-    -- ISH: Something.
-    -- BAD: Something.
-    -- :TodoQuickFix
-    -- :TodoTelescope
-    -- :TodoTrouble
-    {
-        "folke/todo-comments.nvim",
-        requires = "nvim-lua/plenary.nvim",
-        config = function()
-            require("user.todo-comments").config()
-        end
-    },
-    -- Symbols outline - F12
-    {
-        'simrat39/symbols-outline.nvim',
-        cmd = 'SymbolsOutline'
-    },
-    -- Enhanced increment/decrement : True, true, January
-    {
-        "monaqa/dial.nvim",
-        event = "BufRead",
-        config = function()
-            require("user.dial").config()
-        end,
-    },
-    -- SQL LSP.
-    {
-        "nanotee/sqls.nvim",
-        event = "BufRead",
-        ft = "sql",
-        config = function()
-            require("user.sqls").config()
-        end,
-    },
-    -- Markdown preview
-    {
-        "iamcco/markdown-preview.nvim",
-        run = "cd app && npm install",
-        ft = "markdown",
-    },
-    -- Codi: Interactive scratchpad
-    {
-        "metakirby5/codi.vim",
-        cmd = "Codi",
-    },
-    -- Markers in margin. 'ma' adds marker
-    {"kshenoy/vim-signature",
-        event = "BufRead",
-    },
-    -- Surroundings.  Try cs"'  in a string "with double quotes" to convert to single.
-    {
-        "tpope/vim-surround",
-        event = "BufRead",
-    },
-    -- Unix commands. Try ":SudoWrite"
-    {
-        "tpope/vim-eunuch",
-        event = "BufRead",
-    },
-    -- Highlight URL's. http://www.vivaldi.com
-    {
-        "itchyny/vim-highlighturl",
-        event = "BufRead",
-    },
-    -- Git plugin.  Try ":Git "
-    {
-        "tpope/vim-fugitive",
-        event = "BufRead",
-    },
-    -- Lazygit: Try F8
-    {
-        "kdheepak/lazygit.nvim",
-        cmd = "LazyGit",
-    },
-    -- Ranger, Leader r
-    {
-        "kevinhwang91/rnvimr",
-        cmd = "RnvimrToggle",
-    }
-}
--- }}}1
+  },
+  {
+    'chipsenkbeil/distant.nvim',
+    config = function()
+      require('distant').setup {
+        -- Applies Chip's personal settings to every machine you connect to
+        --
+        -- 1. Ensures that distant servers terminate with no connections
+        -- 2. Provides navigation bindings for remote directories
+        -- 3. Provides keybinding to jump into a remote file's parent directory
+        ['*'] = require('distant.settings').chip_default()
+      }
+    end
+  },
+  {
+    "windwp/nvim-spectre",
+    event = "BufRead",
+    config = function()
+      require("spectre").setup()
+    end,
+  },
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+        require('Comment').setup()
+    end
+  },
 
--- Autocommands (https://neovim.io/doc/user/autocmd.html) {{{1
+--     {"folke/tokyonight.nvim"},
+--     {
+--       "folke/trouble.nvim",
+--       cmd = "TroubleToggle",
+--     },
+}
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
 --   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
 -- }
--- }}}1
-
--- Additional Leader bindings for WhichKey and key mappings {{{1
-lvim.builtin.which_key.mappings["r"] = {
-    "<cmd>RnvimrToggle<CR>",                             "Ranger"
-}
-
-lvim.builtin.which_key.mappings["z"] = {
-    "<cmd>ZenMode<CR>",                             "Zen Mode"
-}
-
-lvim.builtin.which_key.mappings["t"] = {
-    name = "Toggle Display Options",
-    a = { ":call ToggleAll()<CR>",                         "Toggle All"},
-    b = { ":call ToggleColourGitBlame()<CR>",              "Toggle git Blame line visibility"},
-    c = { ":call ToggleColourCursorColumn()<CR>",          "Toggle cursor Column visibility"},
-    e = { ":call ToggleColourWhiteSpaceAtEndOfLine()<CR>", "Toggle whitespace at End of line visibility"},
-    g = { ":call ToggleGutter()<CR>",                      "Toggle left Gutter"},
-    h = { ":ColorizerToggle<CR>",                          "Toggle Hex colour and colour name matches"},
-    i = { ":call ToggleColourIncSearch()<CR>",             "Toggle hIghlight matching words under cursor"},
-    l = { ":call ToggleColourCursorLine()<CR>",            "Toggle cursor Line visibility"},
-    m = { ":call ToggleMarkerLines()<CR>",                 "Toggle Marker lines"},
-    r = { ":set wrap!<CR>",                                "Toggle line wRap"},
-    s = { ":set spell!<CR>",                               "Toggle Spell checking"},
-    t = { ":call ToggleColourLineTooLong()<CR>",           "Toggle line Too long highlighting"},
-    w = { ":set list!<CR>",                                "Toggle Whitespace visibility"},
-    y = { ":call ToggleColourSyntax()<CR>",                "Toggle sYntax highlighting"}
-}
-lvim.builtin.which_key.mappings["j"] = {
-    name = "Show/Jump various dev info",
-    a = {":silent exec '!jump Artifact %:p:h'<CR>",        "Jump Artifactory (project)"},
-    c = {":silent exec '!jump Ci %:p:h'<CR>",              "Jump Ci (project)"},
-    h = {":call OpenHelpPage()<CR>",                       "Jump vim Help page for word under cursor"},
-    i = {":silent exec '!jump Live %:p:h'<CR>",            "Jump lIve (project)"},
-    j = {":call JumpToSelection()<CR>",                    "Jump to url or hex color or git etc"},
-    l = {":silent exec '!jump Lint %:p:h'<CR>",            "Jump Lint (project)"},
-    m = {":<C-U>exe 'Man' v:count '<C-R><C-W>'<CR>",       "Jump linux Man page for word under cursor"},
-    n = {":silent exec '!jump Notes %:p:h'<CR>",           "Jump Notes (project search in confluence)"},
-    r = {":silent exec '!jump Repo %:p:h'<CR>",            "Jump Repo (git or bitbucket)"},
-    s = {":call ShowJira()<CR>",                           "Show jira ticket in new buffer"},
-    t = {":silent exec '!jump Ticket %:p:h'<CR>",          "Jump jira Ticket in browser"},
-    u = {":silent exec '!jump TestReports %:p:h'<CR>",     "Jump Unit test reports"}
-}
-lvim.builtin.which_key.mappings["d"] = {
-    name = "Diagnostics",
-    t = { "<cmd>TroubleToggle<CR>", "trouble" },
-    w = { "<cmd>TroubleToggle lsp_workspace_diagnostics<CR>", "workspace" },
-    d = { "<cmd>TroubleToggle lsp_document_diagnostics<CR>", "document" },
-    q = { "<cmd>TroubleToggle quickfix<CR>", "quickfix" },
-    l = { "<cmd>TroubleToggle loclist<CR>", "loclist" },
-    r = { "<cmd>TroubleToggle lsp_references<CR>", "references" },
-}
-
-vim.api.nvim_set_keymap("n", "<TAB>",   ":BufferNext<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<S-TAB>", ":BufferPrevious<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<S-l>",   ":BufferNext<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<S-h>",   ":BufferPrevious<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<S-x>",   ":BufferClose<CR>", { noremap = true, silent = true })
-
--- }}}1
-
-vim.cmd('source ~/.config/lvim/user.vim')
-vim.cmd('source ~/.config/lvim/lua/user/lualine.lua')
-
--- See end of this file for my other config.
--- ~/.local/share/lunarvim/lvim/init.lua
--- Two lines added:
---     vim.cmd('source ~/.config/lvim/user_colors.vim')
---     vim.cmd('source ~/.config/lvim/user_keys.vim')
-
-
