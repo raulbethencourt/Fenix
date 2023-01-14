600c489732bce552aa3c5ae8cdb704a9157b9135
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2023-01-14 20:07:58 +0100

    :wrench: tmux alacritty

--- dotfiles/zsh/exports.zsh
+++ dotfiles/zsh/exports.zsh
@@ -1,16 +1,18 @@
 # General
 export TERM="xterm-256color"
+export NVCHAD_CACHE_DIR="~/.cache/nvim/project_nvim"
 
 # FZF color theme
 export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' 
-    --color fg:-1,bg:-1,hl:230,fg+:3,bg+:233,hl+:229
-    --color info:150,prompt:110,spinner:150,pointer:167,marker:174'
+--color fg:-1,bg:-1,hl:230,fg+:3,bg+:233,hl+:229
+--color info:150,prompt:110,spinner:150,pointer:167,marker:174'
 
 # PATHS
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.fzf/bin:$PATH"
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export PATH="$HOME/.config/composer/vendor/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 export PATH="$PATH:$HOME/.composer/vendor/bin"
