0de78d995a3b8750ad5dc22e6f041d551b48b2de
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2023-03-14 22:35:02 +0100

    :construction: new scripts and lvim config

---
+++ scripts/cht.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+languages=`echo "bash rust php lua nodejs typescript" | tr ' ' '\n'`
+core_utils=`echo "read xargs find fd mv sed awk rg grep tail df" | tr ' ' '\n'`
+
+selected=`printf "$languages\n$core_utils" | fzf`
+read "query?query: " 
+
+if printf $languages | grep -qs $selected; then
+    tmux neww zsh -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
+else
+    tmux neww zsh -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
+fi
