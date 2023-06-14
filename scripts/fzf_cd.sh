330f7fed0d079f421f0a231d0e1ee662a951c1a1
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2023-06-14 20:58:59 +0200

    :hammer: Delete scripts coz they are in tools repo

--- scripts/fzf_cd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd
-
-FZF_RESULT=$(fd --type d --hidden --exclude .git | fzf --preview "bat --color=always --style=numbers --line-range=:500 {}")
-
-if [[ $FZF_RESULT ]]; then
-	cd "$HOME/${FZF_RESULT:2}"
-fi
