2b957470982605db7bcb6a3f6baedc3eb7f4ebea
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2023-07-06 23:04:36 +0200

    :wrench: Lvim

--- dotfiles/.config/lvim/lua/raBeta/plugins.lua
+++ dotfiles/.config/lvim/lua/raBeta/plugins.lua
@@ -10,14 +10,12 @@ lvim.plugins = {
 	"MunifTanjim/nui.nvim",
 	"eddyekofo94/gruvbox-flat.nvim",
 	"ThePrimeagen/harpoon",
-	"ThePrimeagen/vim-be-good",
 	"mbbill/undotree",
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
 		dependencies = {
 			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
 		},
 	},
 	{ "rose-pine/neovim", name = "rose-pine" },
@@ -25,10 +23,10 @@ lvim.plugins = {
 		"aserowy/tmux.nvim",
 		config = true,
 	},
-	{
-		"folke/zen-mode.nvim",
-		config = true,
-	},
+	-- {
+	-- 	"folke/zen-mode.nvim",
+	-- 	config = true,
+	-- },
 	{
 		"saecki/crates.nvim",
 		event = { "BufRead Cargo.toml" },
@@ -68,89 +66,6 @@ lvim.plugins = {
 		"windwp/nvim-ts-autotag",
 		config = function()
 			require("nvim-ts-autotag").setup()
-		end,
-	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v2.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-		config = function()
-			require("neo-tree").setup({
-				close_if_last_window = true,
-				window = {
-					width = 30,
-					mappings = {
-						["<space>"] = {
-							"toggle_node",
-							nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
-						},
-						["<esc>"] = "revert_preview",
-						["P"] = { "toggle_preview", config = { use_float = true } },
-						["l"] = "focus_preview",
-						["S"] = "open_split",
-						["s"] = "open_vsplit",
-						-- ["S"] = "split_with_window_picker",
-						-- ["s"] = "vsplit_with_window_picker",
-						["t"] = "open_tabnew",
-						-- ["<cr>"] = "open_drop",
-						-- ["t"] = "open_tab_drop",
-						["w"] = "open_with_window_picker",
-						--["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
-						["C"] = "close_node",
-						-- ['C'] = 'close_all_subnodes',
-						["z"] = "close_all_nodes",
-						--["Z"] = "expand_all_nodes",
-						["a"] = {
-							"add",
-							-- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
-							-- some commands may take optional config options, see `:h neo-tree-mappings` for details
-							config = {
-								show_path = "none", -- "none", "relative", "absolute"
-							},
-						},
-						["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
-						["d"] = "delete",
-						["r"] = "rename",
-						["y"] = "copy_to_clipboard",
-						["x"] = "cut_to_clipboard",
-						["p"] = "paste_from_clipboard",
-						["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
-						-- ["c"] = {
-						--  "copy",
-						--  config = {
-						--    show_path = "none" -- "none", "relative", "absolute"
-						--  }
-						--}
-						["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
-						["q"] = "close_window",
-						["R"] = "refresh",
-						["?"] = "show_help",
-						["<"] = "prev_source",
-						[">"] = "next_source",
-					},
-				},
-				buffers = {
-					follow_current_file = true,
-				},
-				filesystem = {
-					follow_current_file = true,
-					filtered_items = {
-						hide_dotfiles = false,
-						hide_gitignored = false,
-						hide_by_name = {
-							"node_modules",
-						},
-						never_show = {
-							".DS_Store",
-							"thumbs.db",
-						},
-					},
-				},
-			})
 		end,
 	},
 	{
