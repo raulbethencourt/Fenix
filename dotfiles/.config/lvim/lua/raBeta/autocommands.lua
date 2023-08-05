47ff1b02f85b67780bbbbe076700779983640a09
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2023-08-05 04:22:13 +0200

    :wrench: Lvim

--- dotfiles/.config/lvim/lua/raBeta/autocommands.lua
+++ dotfiles/.config/lvim/lua/raBeta/autocommands.lua
@@ -1,23 +1,23 @@
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
-vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = { "*", },
-    -- hidde lualine
-    command = "set ls=0",
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*" },
+	-- hidde lualine
+	command = "set ls=0",
 })
-vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = { "*", },
-    -- enable wrap mode for json files only
-    command = "set fcs=eob:\\ ",
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*" },
+	-- enable wrap mode for json files only
+	command = "set fcs=eob:\\ ",
 })
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "zsh",
-    callback = function()
-        -- let treesitter use bash highlight for zsh files as well
-        require("nvim-treesitter.highlight").attach(0, "bash")
-    end,
+	pattern = "zsh",
+	callback = function()
+		-- let treesitter use bash highlight for zsh files as well
+		require("nvim-treesitter.highlight").attach(0, "bash")
+	end,
 })
-vim.api.nvim_create_autocmd("VimEnter", {
-    pattern = { "*", },
-    -- hidde vim tabline
-    command = "set showtabline=0 ",
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*" },
+	-- hidde vim tabline
+	command = "set showtabline=0 ",
 })
