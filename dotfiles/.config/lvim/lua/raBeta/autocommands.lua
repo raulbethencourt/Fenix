3dfaed5d47dd1709c8654b65d0d13648c85c1e40
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2023-09-12 23:05:43 +0200

    :wrench: Lvim

--- dotfiles/.config/lvim/lua/raBeta/autocommands.lua
+++ dotfiles/.config/lvim/lua/raBeta/autocommands.lua
@@ -1,8 +1,18 @@
+-- vim-dadbod-completion
+local autocomplete_group = vim.api.nvim_create_augroup("vimrc_autocompletion", { clear = true })
+local cmp = require("cmp")
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "sql", "mysql", "plsql" },
 	callback = function()
-		require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+		cmp.setup.buffer({
+			sources = {
+				{ name = "vim-dadbod-completion" },
+				{ name = "buffer" },
+				{ name = "luasnip" },
+			},
+		})
 	end,
+	group = autocomplete_group,
 })
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 	pattern = { "*" },
