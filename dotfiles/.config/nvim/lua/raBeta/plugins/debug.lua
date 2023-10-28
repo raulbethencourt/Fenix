132ac139276f9d45786db25f42129bd44190daa9
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2023-10-28 23:47:35 +0200

    :wrench: Nvim dap

--- dotfiles/.config/nvim/lua/raBeta/plugins/debug.lua
+++ dotfiles/.config/nvim/lua/raBeta/plugins/debug.lua
@@ -1,61 +1,139 @@
 return {
---   'mfussenegger/nvim-dap',
---   dependencies = {
---     'rcarriga/nvim-dap-ui',
---
---     'williamboman/mason.nvim',
---     'jay-babu/mason-nvim-dap.nvim',
---
---     'leoluz/nvim-dap-go',
---   },
---   config = function()
---     local dap = require 'dap'
---     local dapui = require 'dapui'
---
---     require('mason-nvim-dap').setup {
---       automatic_setup = true,
---
---       handlers = {},
---
---       ensure_installed = {
---         'delve',
---       },
---     }
---
---     vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
---     vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
---     vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
---     vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
---     vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
---     vim.keymap.set('n', '<leader>dB', function()
---       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
---     end, { desc = 'Debug: Set Breakpoint' })
---
---     dapui.setup {
---       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
---       controls = {
---         icons = {
---           pause = '⏸',
---           play = '▶',
---           step_into = '⏎',
---           step_over = '⏭',
---           step_out = '⏮',
---           step_back = 'b',
---           run_last = '▶▶',
---           terminate = '⏹',
---           disconnect = '⏏',
---         },
---       },
---     }
---
---     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
---     vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
---
---     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
---     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
---     dap.listeners.before.event_exited['dapui_config'] = dapui.close
---
---     -- Install golang specific config
---     require('dap-go').setup()
---   end,
+  'mfussenegger/nvim-dap',
+  dependencies = {
+    'rcarriga/nvim-dap-ui',
+
+    'williamboman/mason.nvim',
+    'jay-babu/mason-nvim-dap.nvim',
+
+    'leoluz/nvim-dap-go',
+  },
+  config = function()
+    local dap = require 'dap'
+    local dapui = require 'dapui'
+
+    require('mason-nvim-dap').setup {
+      automatic_setup = true,
+
+      handlers = {},
+
+      ensure_installed = {
+        'delve',
+      },
+    }
+
+    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+    vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+    vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+    vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+    vim.keymap.set('n', '<leader>dB', function()
+      dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+    end, { desc = 'Debug: Set Breakpoint' })
+
+    dapui.setup {
+      active = true,
+      on_config_done = nil,
+      breakpoint = {
+        text = '',
+        texthl = 'DiagnosticSignError',
+        linehl = '',
+        numhl = '',
+      },
+      breakpoint_rejected = {
+        text = '',
+        texthl = 'DiagnosticSignError',
+        linehl = '',
+        numhl = '',
+      },
+      stopped = {
+        text = '',
+        texthl = 'DiagnosticSignWarn',
+        linehl = 'Visual',
+        numhl = 'DiagnosticSignWarn',
+      },
+      log = {
+        level = 'info',
+      },
+      ui = {
+        auto_open = true,
+        notify = {
+          threshold = vim.log.levels.INFO,
+        },
+        config = {
+          icons = { expanded = '', collapsed = '', circular = '' },
+          mappings = {
+            -- Use a table to apply multiple mappings
+            expand = { '<CR>', '<2-LeftMouse>' },
+            open = 'o',
+            remove = 'd',
+            edit = 'e',
+            repl = 'r',
+            toggle = 't',
+          },
+          -- Use this to override mappings for specific elements
+          element_mappings = {},
+          expand_lines = true,
+          layouts = {
+            {
+              elements = {
+                { id = 'scopes', size = 0.33 },
+                { id = 'breakpoints', size = 0.17 },
+                { id = 'stacks', size = 0.25 },
+                { id = 'watches', size = 0.25 },
+              },
+              size = 0.33,
+              position = 'right',
+            },
+            {
+              elements = {
+                { id = 'repl', size = 0.45 },
+                { id = 'console', size = 0.55 },
+              },
+              size = 0.27,
+              position = 'bottom',
+            },
+          },
+          controls = {
+            enabled = true,
+            -- Display controls in this element
+            element = 'repl',
+            icons = {
+              pause = '',
+              play = '',
+              step_into = '',
+              step_over = '',
+              step_out = '',
+              step_back = '',
+              run_last = '',
+              terminate = '',
+            },
+          },
+          floating = {
+            max_height = 0.9,
+            max_width = 0.5, -- Floats will be treated as percentage of your screen.
+            border = 'rounded',
+            mappings = {
+              close = { 'q', '<Esc>' },
+            },
+          },
+          windows = { indent = 1 },
+          render = {
+            max_type_length = nil, -- Can be integer or nil.
+            max_value_lines = 100, -- Can be integer or nil.
+          },
+        },
+      },
+    }
+
+    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+    vim.keymap.set('n', '<leader>dt', dapui.toggle, { desc = 'Debug: See last session result.' })
+
+    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+    -- Install golang specific config
+    require('dap-go').setup()
+  end,
 }
