a46a59a4c1eaaf0107a4a88c62431a8de5fe8092
raul-bns <raul.bethencourt@bluenot-systems.com>
2023-12-24 16:51:34 +0100

    [config] preparing for personal computer reinstallation

--- dotfiles/.config/nvim/lua/raBeta/configs/lsp/init.lua
+++ dotfiles/.config/nvim/lua/raBeta/configs/lsp/init.lua
@@ -112,13 +112,27 @@ local servers = {
 -- disblae inline hints
 vim.diagnostic.config {
   virtual_text = false,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = ' ',
+      [vim.diagnostic.severity.WARN] = ' ',
+      [vim.diagnostic.severity.HINT] = ' ',
+      [vim.diagnostic.severity.INFO] = ' ',
+    },
+    linehl = {
+      [vim.diagnostic.severity.ERROR] = '',
+      [vim.diagnostic.severity.WARN] = '',
+      [vim.diagnostic.severity.HINT] = '',
+      [vim.diagnostic.severity.INFO] = '',
+    },
+    numhl = {
+      [vim.diagnostic.severity.WARN] = 'WarningMsg',
+      [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+      [vim.diagnostic.severity.HINT] = 'HintMsg',
+      [vim.diagnostic.severity.INFO] = 'InfoMsg',
+    },
+  },
 }
-
-local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
-for type, icon in pairs(signs) do
-  local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
 
 require('neodev').setup()
 
