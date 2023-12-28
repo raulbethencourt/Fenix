5c995619e8bbd878d474d5e636b58dcfcc23383d
raulbethencourt <raul.bethencourt.pro@gmail.com>
2023-12-28 21:52:04 +0100

    [scripts] working in auto install

--- install.sh
+++ install.sh
@@ -8,48 +8,83 @@ sudo add-apt-repository ppa:ubuntuhandbook1/keepass2
 
 # github cli
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+  sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
 
 # alacritty
 sudo add-apt-repository ppa:aslatter/ppa -y
 
+# brave-browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+# vivaldi
+wget -O- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/vivaldi.gpg
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/vivaldi.gpg] https://repo.vivaldi.com/archive/deb/ stable main | sudo tee /etc/apt/sources.list.d/vivaldi.list
+
+# neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+
+# keevault
+sudo add-apt-repository ppa:dlech/keepass2-plugins
+
 set -eu -o pipefail # fail on error and report it, debug all lines
 
 sudo -n true
-test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
+[ $? -eq 0 ] || exit 1 "you should have sudo privilege to run this script"
 
-sudo apt update -y
+sudo apt install nala
+
+sudo nala update
 
 echo installing the must-have pre-requisites
-while read -r p; do sudo apt install -y $p; done < <(
-	cat <<"EOF"
-	nala
+while read -r p; do sudo nala install -y $p; done < <(
+  cat <<"EOF"
+	wget 
+	make
+	gnupg2 
+	ubuntu-keyring
 	alacritty
-    curl
+	zsh
+	curl
 	tmux
-    wget
-    ffmpeg
-    code
+	brave-browser
+	vivaldi-stable
+	wget
+	ffmpeg
+	vim
+	neovim
 	gh
-    vlc
-    filezilla
-    steam
-    neofetch
+	vlc
+	filezilla
+	steam
+	neofetch
 	bat
 	duf
 	fzf
 	zoxide
 	ripgrep
-    fd-find
-    exa 
-    zathura
-    ascii-image-converter
-    gim
+	fd-find
+	exa 
+	zathura
+	ascii-image-converter
+	gimp
+	libimlib2-dev
+	libxft-dev
 	keepass2
-	tweaks
+	gnome-tweaks
 	tig
+	xorg 
+	btop
+	openbox
+	freetype2-doc
+	fontconfig
+	libgif-dev
+	libexif-gtk5
+	eza
+	golang-go
+	keepass2-plugin-rpc
 EOF
 )
 
@@ -60,23 +95,20 @@ echo hit Ctrl+C to quit
 echo -e "\n"
 sleep 6
 
-# Recover config from Fenix repo
-gh repo clone raulbethencourt/Fenix
-cp -fr "$HOME"/Fenix/dotfiles/* "$HOME"/
-cp -fr "$HOME"/Fenix/dotfiles/.config/* "$HOME"/.config/
+# make zsh shell
+chsh -s $(which zsh)
 
-# Dragon
-gh repo clone mwh/dragon "$HOME"/git_apps/dragon
-cd "$HOME"/git_apps/dragon
-make install
-cd
+# zap !!!
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
 
-# sxiv
-gh repo clone xyb3rt/sxiv "$HOME"/git_apps/sxiv
-cd "$HOME"/git_apps/sxiv
-make install
-cd
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # link fd bin
 ln -s "$(which fdfind)" ~/.local/bin/fd
 
+# bat
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
+
+echo "Finish first install, reboot your system to persiste changes..."
