619701b63b07490a39a29aa22e7fad4d088c469d
raul-bns <raul.bethencourt@bluenot-systems.com>
2024-01-30 08:56:13 +0100

    [config] nvim for vscode

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -1,25 +1,49 @@
--- NOTE: need to set leader before lazy
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- NOTE: vscode nvim settings
+if vim.g.vscode then
+  require 'vscode.mappings_vscode'
 
--- NOTE: install lazy
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
+  local cmd = {
+    [[source $HOME/.config/nvim/vscode/vim_settings.vim]],
+    [[source $HOME/.config/nvim/vscode/functions.vim]],
+    [[source $HOME/.config/nvim/plug-config/highlightyank.vim]],
+    [[source $HOME/.config/nvim/plug-config/polyglot.vim]],
+    [[source $HOME/.config/nvim/plug-config/quickscope.vim]],
+    [[source $HOME/.config/nvim/vim-plug/plugins.vim]],
+    [[source $HOME/.config/nvim/vscode/settings.vim]],
+    [[nnoremap z= <Cmd>call VSCodeNotify('keyboard-quickfix.openQuickFix')<CR>]],
   }
+
+  ---@diagnostic disable-next-line: unused-local
+  for i, v in ipairs(cmd) do
+    vim.cmd(v)
+  end
+
+  require 'vscode.hop'
+  require 'vscode.nvim-surround'
+else
+  -- NOTE: need to set leader before lazy
+  vim.g.mapleader = ' '
+  vim.g.maplocalleader = ' '
+
+  -- NOTE: install lazy
+  local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system {
+      'git',
+      'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable',
+      lazypath,
+    }
+  end
+  vim.opt.rtp:prepend(lazypath)
+
+  -- NOTE: load plugins
+  require('lazy').setup({
+    { import = 'raBeta.plugins' },
+  }, {})
+
+  -- NOTE: load configs
+  require 'raBeta.configs'
 end
-vim.opt.rtp:prepend(lazypath)
-
--- NOTE: load plugins
-require('lazy').setup({
-  { import = 'raBeta.plugins' },
-}, {})
-
--- NOTE: load configs
-require 'raBeta.configs'
---
+++ dotfiles/.config/nvim/lua/vscode/nvim-surround.lua
@@ -0,0 +1,2 @@
+-- NOTE: start config to use it in vscode
+require("nvim-surround").setup()
