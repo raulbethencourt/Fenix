3d7181d2771f1eee8b1b3f5a52d844a8ac65b89f
raul-bns <raul.bethencourt@bluenot-systems.com>
2024-01-31 16:41:56 +0100

    [config] nvim use lazy for vscode also

---
+++ dotfiles/.config/nvim/lua/vscode/plugins.lua
@@ -0,0 +1,10 @@
+return {
+    'tpope/vim-commentary',
+    'tpope/vim-speeddating',
+    'glts/vim-radical',
+    'tpope/vim-repeat',
+    'suy/vim-context-commentstring',
+    'folke/flash.nvim',
+    'machakann/vim-highlightedyank',
+    'kylechui/nvim-surround',
+}
