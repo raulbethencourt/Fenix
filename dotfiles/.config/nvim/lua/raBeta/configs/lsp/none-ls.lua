c81320fe5351fb4d841afc04c4933dfc2b55d0e2
raulbethencourt <raul.bethencourt.pro@gmail.com>
2024-02-07 22:00:36 +0100

    [config] nvim last change from year

--- dotfiles/.config/nvim/lua/raBeta/configs/lsp/none-ls.lua
+++ dotfiles/.config/nvim/lua/raBeta/configs/lsp/none-ls.lua
@@ -1,57 +1,59 @@
 local nls = require 'null-ls'
 
 nls.setup {
-	debounce = 150,
-	save_after_format = false,
-	sources = {
-		-- Formatting
-		nls.builtins.formatting.stylua,
-		nls.builtins.formatting.fish_indent,
-		nls.builtins.formatting.shfmt.with {
-			filetypes = { 'sh', 'zsh', 'bash' },
-			extra_args = { '-i', '2' },
-		},
-		nls.builtins.formatting.isort,
-		nls.builtins.formatting.rustfmt,
-		nls.builtins.formatting.pretty_php,
-		nls.builtins.formatting.black,
-		nls.builtins.formatting.clang_format.with {
-			extra_args = { '-style', '{IndentWidth: 4}' },
-		},
-		nls.builtins.formatting.prettier.with {
-			extra_args = { '--print-with=100', '--tab-width=4' },
-			filetypes = {
-				'typescript',
-				'typescriptreact',
-				'scss',
-				'css',
-				'html',
-				'twig',
-				'javascript',
-				'json',
-			},
-		},
+  debounce = 150,
+  save_after_format = false,
+  sources = {
+    -- Formatting
+    nls.builtins.formatting.stylua,
+    nls.builtins.formatting.fish_indent,
+    nls.builtins.formatting.shfmt.with {
+      filetypes = { 'sh', 'zsh', 'bash' },
+      extra_args = { '-i', '2' },
+    },
+    nls.builtins.formatting.isort,
+    nls.builtins.formatting.mdformat,
+    nls.builtins.formatting.rustfmt,
+    nls.builtins.formatting.pretty_php,
+    nls.builtins.formatting.black,
+    nls.builtins.formatting.clang_format.with {
+      extra_args = { '-style', '{IndentWidth: 4}' },
+    },
+    nls.builtins.formatting.prettier.with {
+      extra_args = { '--print-with=100', '--tab-width=4' },
+      filetypes = {
+        'typescript',
+        'typescriptreact',
+        'scss',
+        'css',
+        'html',
+        'twig',
+        'javascript',
+        'json',
+        'php',
+      },
+    },
 
-		-- Lintings
-		nls.builtins.diagnostics.markdownlint,
-		nls.builtins.diagnostics.shellcheck.with {
-			filetypes = { 'sh', 'zsh', 'bash' },
-		},
-		nls.builtins.diagnostics.flake8,
-		nls.builtins.diagnostics.phpstan,
-		nls.builtins.diagnostics.codespell.with {
-			filetypes = {
-				'javascript',
-				'python',
-				'rust',
-				'python',
-				'php',
-				'sh',
-				'zsh',
-				'bash',
-			},
-		},
-		nls.builtins.diagnostics.cpplint,
-	},
-	root_dir = require('null-ls.utils').root_pattern('.null-ls-root', '.neoconf.json', '.git'),
+    -- Lintings
+    nls.builtins.diagnostics.markdownlint,
+    nls.builtins.diagnostics.shellcheck.with {
+      filetypes = { 'sh', 'zsh', 'bash' },
+    },
+    nls.builtins.diagnostics.flake8,
+    nls.builtins.diagnostics.phpstan,
+    nls.builtins.diagnostics.codespell.with {
+      filetypes = {
+        'javascript',
+        'python',
+        'rust',
+        'python',
+        'php',
+        'sh',
+        'zsh',
+        'bash',
+      },
+    },
+    nls.builtins.diagnostics.cpplint,
+  },
+  root_dir = require('null-ls.utils').root_pattern('.null-ls-root', '.neoconf.json', '.git'),
 }
