7d3023859949eaa6cad78507cabda4e0629515d9
raulbethencourt <raul.bethencourt.pro@gmail.com>
2024-02-20 23:15:25 +0100

    [config] nvim

--- dotfiles/.config/nvim/lua/raBeta/configs/lsp/languages/rust.lua
+++ dotfiles/.config/nvim/lua/raBeta/configs/lsp/languages/rust.lua
@@ -6,8 +6,7 @@ if ok then
   local codelldb = mason_registry.get_package 'codelldb'
   local extension_path = codelldb:get_install_path() .. '/extension/'
   local codelldb_path = extension_path .. 'adapter/codelldb'
-  local liblldb_path = vim.fn.has 'mac' == 1 and extension_path .. 'lldb/lib/liblldb.dylib' or
-  extension_path .. 'lldb/lib/liblldb.so'
+  local liblldb_path = vim.fn.has 'mac' == 1 and extension_path .. 'lldb/lib/liblldb.dylib' or extension_path .. 'lldb/lib/liblldb.so'
   adapter = require('rust-tools.dap').get_codelldb_adapter(codelldb_path, liblldb_path)
 end
 
