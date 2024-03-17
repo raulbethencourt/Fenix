3e17187c448c7fd94aad019ed0f24e9e8aced5af
raulbethencourt <raul.bethencourt.pro@gmail.com>
2024-03-17 21:45:24 +0100

    [nvim] change intelephense by phpactor

--- dotfiles/.config/nvim/lua/raBeta/configs/lsp/init.lua
+++ dotfiles/.config/nvim/lua/raBeta/configs/lsp/init.lua
@@ -5,217 +5,217 @@ require 'raBeta.configs.lsp.languages.php'
 -- NOTE: stop saving lsp logs, change to 'debug' to see them
 vim.lsp.set_log_level 'off'
 local on_attach = function(_, bufnr)
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
+    local nmap = function(keys, func, desc)
+        if desc then
+            desc = 'LSP: ' .. desc
+        end
+
+        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
     end
 
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
+    nmap('gr', '<cmd>Telescope lsp_references<CR>', '[G]oto [R]eferences')
+    nmap('gd', '<cmd>Telescope lsp_definitions<CR>', '[G]oto [D]definition')
+    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+    nmap('gl', '<cmd>lua vim.diagnostic.open_float()<CR>', '[G]oto [L]ine diagnostics')
+    nmap('<leader>ld', vim.lsp.buf.type_definition, '[L]sp Type [D]efinition')
 
-  nmap('gr', '<cmd>Telescope lsp_references<CR>', '[G]oto [R]eferences')
-  nmap('gd', '<cmd>Telescope lsp_definitions<CR>', '[G]oto [D]definition')
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-  nmap('gl', '<cmd>lua vim.diagnostic.open_float()<CR>', '[G]oto [L]ine diagnostics')
-  nmap('<leader>ld', vim.lsp.buf.type_definition, '[L]sp Type [D]efinition')
+    nmap('<leader>lf', function()
+        vim.lsp.buf.format { async = true }
+    end, '[L]sp [F]ormat')
 
-  nmap('<leader>lf', function()
-    vim.lsp.buf.format { async = true }
-  end, '[L]sp [F]ormat')
+    nmap('<leader>lD', '<cmd>Telescope diagnostics<CR>', '[L]sp Telescope Workspace [D]iagnostics')
 
-  nmap('<leader>lD', '<cmd>Telescope diagnostics<CR>', '[L]sp Telescope Workspace [D]iagnostics')
+    nmap('<leader>lb', function()
+        require('telescope.builtin').diagnostics(require('telescope.themes').get_dropdown {
+            winblend = 0,
+            previewer = true,
+            border = false,
+            layout_strategy = 'vertical',
+            layout_config = {
+                height = 0.5,
+                prompt_position = 'top',
+                width = 0.4,
+                preview_height = 0.6,
+            },
+            bufnr = 0,
+            no_sign = true,
+        })
+    end, '[L]sp Telescope [B]uffer Diagnostics')
 
-  nmap('<leader>lb', function()
-    require('telescope.builtin').diagnostics(require('telescope.themes').get_dropdown {
-      winblend = 0,
-      previewer = true,
-      border = false,
-      layout_strategy = 'vertical',
-      layout_config = {
-        height = 0.5,
-        prompt_position = 'top',
-        width = 0.4,
-        preview_height = 0.6,
-      },
-      bufnr = 0,
-      no_sign = true,
-    })
-  end, '[L]sp Telescope [B]uffer Diagnostics')
+    nmap('<leader>ls', function()
+        require('telescope.builtin').lsp_document_symbols(require('telescope.themes').get_dropdown {
+            winblend = 0,
+            previewer = true,
+            border = false,
+            layout_strategy = 'vertical',
+            layout_config = {
+                height = 0.5,
+                prompt_position = 'top',
+                width = 0.4,
+                preview_height = 0.6,
+            },
+            bufnr = 0,
+            no_sign = true,
+        })
+    end, '[L]sp Document [S]ymbols')
 
-  nmap('<leader>ls', function()
-    require('telescope.builtin').lsp_document_symbols(require('telescope.themes').get_dropdown {
-      winblend = 0,
-      previewer = true,
-      border = false,
-      layout_strategy = 'vertical',
-      layout_config = {
-        height = 0.5,
-        prompt_position = 'top',
-        width = 0.4,
-        preview_height = 0.6,
-      },
-      bufnr = 0,
-      no_sign = true,
-    })
-  end, '[L]sp Document [S]ymbols')
-
-  nmap('<leader>lw', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[L]sp [W]orkspace symbols')
-  nmap('<leader>lr', vim.lsp.buf.rename, '[L]sp [R]ename')
-  nmap('<leader>la', vim.lsp.buf.code_action, '[L]sp code [A]ction')
-  nmap('<leader>ln', vim.lsp.buf.add_workspace_folder, '[L]sp [W]orkspace [A]dd Folder')
-  nmap('<leader>lx', vim.lsp.buf.remove_workspace_folder, '[L]sp [W]orkspace [R]emove Folder')
-  nmap('<leader>lI', '<cmd>Mason<cr>', '[L]sp [I]nstall with mason')
-  nmap('<leader>li', '<cmd>LspInfo<cr>', '[L]sp [I]nfo')
-  nmap('<leader>ll', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[L]sp [W]orkspace [L]ist Folders')
+    nmap('<leader>lw', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[L]sp [W]orkspace symbols')
+    nmap('<leader>lr', vim.lsp.buf.rename, '[L]sp [R]ename')
+    nmap('<leader>la', vim.lsp.buf.code_action, '[L]sp code [A]ction')
+    nmap('<leader>ln', vim.lsp.buf.add_workspace_folder, '[L]sp [W]orkspace [A]dd Folder')
+    nmap('<leader>lx', vim.lsp.buf.remove_workspace_folder, '[L]sp [W]orkspace [R]emove Folder')
+    nmap('<leader>lI', '<cmd>Mason<cr>', '[L]sp [I]nstall with mason')
+    nmap('<leader>li', '<cmd>LspInfo<cr>', '[L]sp [I]nfo')
+    nmap('<leader>ll', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, '[L]sp [W]orkspace [L]ist Folders')
 end
 
 vim.filetype.add {
-  extension = {
-    zsh = 'zsh',
-  },
+    extension = {
+        zsh = 'zsh',
+    },
 }
 
 -- Get intelephense licence
 local get_intelephense_license = function()
-  local f = assert(io.open(os.getenv 'HOME' .. '/intelephense/licence.txt', 'rb'))
-  local content = f:read '*a'
-  f:close()
-  return string.gsub(content, '%s+', '')
+    local f = assert(io.open(os.getenv 'HOME' .. '/intelephense/licence.txt', 'rb'))
+    local content = f:read '*a'
+    f:close()
+    return string.gsub(content, '%s+', '')
 end
 
 local servers = {
-  emmet_ls = {
-    filetypes = { 'twig', 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
-    init_options = {
-      html = {
-        options = {
-          ['bem.enabled'] = true,
+    emmet_ls = {
+        filetypes = { 'twig', 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
+        init_options = {
+            html = {
+                options = {
+                    ['bem.enabled'] = true,
+                },
+            },
         },
-      },
     },
-  },
-  jqls = {
-    filetypes = { 'json', 'jsonc' },
-  },
-  bashls = {
-    filetypes = { 'sh', 'zsh', 'bash' },
-  },
-  clangd = {},
-  rust_analyzer = {
-    settings = {
-      ['rust-analyzer'] = {
-        cargo = {
-          allFeatures = true,
-          loadOutDirsFromCheck = true,
-          runBuildScripts = true,
+    jqls = {
+        filetypes = { 'json', 'jsonc' },
+    },
+    bashls = {
+        filetypes = { 'sh', 'zsh', 'bash' },
+    },
+    clangd = {},
+    rust_analyzer = {
+        settings = {
+            ['rust-analyzer'] = {
+                cargo = {
+                    allFeatures = true,
+                    loadOutDirsFromCheck = true,
+                    runBuildScripts = true,
+                },
+                checkOnSave = {
+                    allFeatures = true,
+                    command = 'clippy',
+                    extraArgs = { '--no-deps' },
+                },
+                procMacro = {
+                    enable = true,
+                    ignored = {
+                        ['async-trait'] = { 'async_trait' },
+                        ['napi-derive'] = { 'napi' },
+                        ['async-recursion'] = { 'async_recursion' },
+                    },
+                },
+            },
         },
-        checkOnSave = {
-          allFeatures = true,
-          command = 'clippy',
-          extraArgs = { '--no-deps' },
+    },
+    taplo = {},
+    marksman = {},
+    html = { filetypes = { 'html', 'twig', 'hbs' } },
+    cssls = {},
+    lua_ls = {
+        settings = {
+            Lua = {
+                runtime = { version = 'LuaJIT' },
+                workspace = {
+                    checkThirdParty = false,
+                    library = {
+                        '${3rd}/luv/library',
+                        unpack(vim.api.nvim_get_runtime_file('', true)),
+                    },
+                },
+                telemetry = { enable = false },
+                format = {
+                    enable = false,
+                },
+                -- diagnostics = { disable = { 'missing-fields' } },
+            },
         },
-        procMacro = {
-          enable = true,
-          ignored = {
-            ['async-trait'] = { 'async_trait' },
-            ['napi-derive'] = { 'napi' },
-            ['async-recursion'] = { 'async_recursion' },
-          },
+    },
+    htmx = { filetypes = { 'html', 'twig', 'php' } },
+    -- intelephense = {
+    --   filetypes = { 'php' },
+    --   init_options = {
+    --     licenceKey = get_intelephense_license(),
+    --     storagePath = '/home/rabeta/.intelephense',
+    --     clearCache = false,
+    --     files = {
+    --       maxSize = 5000000,
+    --     },
+    --     phpMemoryLimit = '4096M',
+    --   },
+    --   diagnostics = {
+    --     enable = true,
+    --   },
+    --   flags = {
+    --     debounce_text_changes = 150,
+    --   },
+    -- },
+    tsserver = {},
+    sqlls = {
+        filetypes = { 'sql' },
+        sql = {
+            'sql-language-server',
+            'up',
+            '--method',
+            'stdio',
         },
-      },
     },
-  },
-  taplo = {},
-  marksman = {},
-  html = { filetypes = { 'html', 'twig', 'hbs' } },
-  cssls = {},
-  lua_ls = {
-    settings = {
-      Lua = {
-        runtime = { version = 'LuaJIT' },
-        workspace = {
-          checkThirdParty = false,
-          library = {
-            '${3rd}/luv/library',
-            unpack(vim.api.nvim_get_runtime_file('', true)),
-          },
-        },
-        telemetry = { enable = false },
-        format = {
-          enable = false,
-        },
-        -- diagnostics = { disable = { 'missing-fields' } },
-      },
-    },
-  },
-  htmx = { filetypes = { 'html', 'twig', 'php' } },
-  intelephense = {
-    filetypes = { 'php' },
-    init_options = {
-      licenceKey = get_intelephense_license(),
-      storagePath = '/home/rabeta/.intelephense',
-      clearCache = false,
-      files = {
-        maxSize = 5000000,
-      },
-      phpMemoryLimit = '4096M',
-    },
-    diagnostics = {
-      enable = true,
-    },
-    flags = {
-      debounce_text_changes = 150,
-    },
-  },
-  tsserver = {},
-  sqlls = {
-    filetypes = { 'sql' },
-    sql = {
-      'sql-language-server',
-      'up',
-      '--method',
-      'stdio',
-    },
-  },
 }
 
 local icons = require 'icons'
 -- disblae inline hints
 vim.diagnostic.config {
-  update_in_insert = true,
-  underline = true,
-  severity_sort = false,
-  float = {
-    border = 'none',
-    source = 'always',
-    header = '',
-    prefix = '',
-  },
-  virtual_text = false,
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = icons.diagnostics.Error .. ' ',
-      [vim.diagnostic.severity.WARN] = icons.diagnostics.Warning .. ' ',
-      [vim.diagnostic.severity.HINT] = icons.diagnostics.Hint .. ' ',
-      [vim.diagnostic.severity.INFO] = icons.diagnostics.Information .. ' ',
+    update_in_insert = true,
+    underline = true,
+    severity_sort = false,
+    float = {
+        border = 'none',
+        source = 'always',
+        header = '',
+        prefix = '',
     },
-    linehl = {
-      [vim.diagnostic.severity.ERROR] = '',
-      [vim.diagnostic.severity.WARN] = '',
-      [vim.diagnostic.severity.HINT] = '',
-      [vim.diagnostic.severity.INFO] = '',
+    virtual_text = false,
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = icons.diagnostics.Error .. ' ',
+            [vim.diagnostic.severity.WARN] = icons.diagnostics.Warning .. ' ',
+            [vim.diagnostic.severity.HINT] = icons.diagnostics.Hint .. ' ',
+            [vim.diagnostic.severity.INFO] = icons.diagnostics.Information .. ' ',
+        },
+        linehl = {
+            [vim.diagnostic.severity.ERROR] = '',
+            [vim.diagnostic.severity.WARN] = '',
+            [vim.diagnostic.severity.HINT] = '',
+            [vim.diagnostic.severity.INFO] = '',
+        },
+        numhl = {
+            [vim.diagnostic.severity.WARN] = 'WarningMsg',
+            [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+            [vim.diagnostic.severity.HINT] = 'HintMsg',
+            [vim.diagnostic.severity.INFO] = 'InfoMsg',
+        },
     },
-    numhl = {
-      [vim.diagnostic.severity.WARN] = 'WarningMsg',
-      [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
-      [vim.diagnostic.severity.HINT] = 'HintMsg',
-      [vim.diagnostic.severity.INFO] = 'InfoMsg',
-    },
-  },
 }
 
 require('neodev').setup()
@@ -226,30 +226,30 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 local mason_lspconfig = require 'mason-lspconfig'
 
 local function rust_opts(name)
-  local plugin = require('lazy.core.config').plugins[name]
-  if not plugin then
-    return {}
-  end
-  local Plugin = require 'lazy.core.plugin'
-  return Plugin.values(plugin, 'opts', false)
+    local plugin = require('lazy.core.config').plugins[name]
+    if not plugin then
+        return {}
+    end
+    local Plugin = require 'lazy.core.plugin'
+    return Plugin.values(plugin, 'opts', false)
 end
 
 mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-  rust_analyzer = function(_, opts)
-    local rust_tools_opts = rust_opts 'rust-tools.nvim'
-    require('rust-tools').setup(vim.tbl_deep_extend('force', rust_tools_opts or {}, { server = opts }))
-    return true
-  end,
+    ensure_installed = vim.tbl_keys(servers),
+    rust_analyzer = function(_, opts)
+        local rust_tools_opts = rust_opts 'rust-tools.nvim'
+        require('rust-tools').setup(vim.tbl_deep_extend('force', rust_tools_opts or {}, { server = opts }))
+        return true
+    end,
 }
 
 mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end,
+    function(server_name)
+        require('lspconfig')[server_name].setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = servers[server_name],
+            filetypes = (servers[server_name] or {}).filetypes,
+        }
+    end,
 }
