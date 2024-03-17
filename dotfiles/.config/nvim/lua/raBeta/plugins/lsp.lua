0e81e4575f7817d2d3e0c5e8be8a80074c016763
raulbethencourt <raul.bethencourt.pro@gmail.com>
2024-03-17 21:45:24 +0100

    [nvim] change intelephense by phpactor

--- dotfiles/.config/nvim/lua/raBeta/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/raBeta/plugins/lsp.lua
@@ -1,133 +1,160 @@
 return {
-  {
-    'Exafunction/codeium.nvim',
-    event = 'BufEnter',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'hrsh7th/nvim-cmp',
-      'onsails/lspkind.nvim',
-    },
-    config = true,
-  },
-  {
-    'kosayoda/nvim-lightbulb',
-    event = 'LspAttach',
-    config = function()
-      require('nvim-lightbulb').setup {
-        autocmd = { enabled = true },
-        sign = {
-          enabled = true,
-          text = require('icons').diagnostics.BoldHint,
-          hl = 'LightBulbSign',
+    {
+        "gbprod/phpactor.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim", -- required to update phpactor
+            "neovim/nvim-lspconfig"  -- required to automatically register lsp serveur
         },
-      }
-    end,
-  },
-  {
-    'nvimtools/none-ls.nvim',
-    event = 'VeryLazy',
-    requires = { 'nvim-lua/plenary.nvim' },
-  },
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
-      'folke/neodev.nvim',
-    },
-  },
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-nvim-lua',
-      'rafamadriz/friendly-snippets',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-nvim-lsp-signature-help',
-      'hrsh7th/cmp-vsnip',
-    },
-  },
-  {
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    build = ':TSUpdate',
-  },
-  {
-    'gbprod/php-enhanced-treesitter.nvim',
-    dependencies = {
-      { 'derekstride/tree-sitter-sql', build = ':TSInstall sql' },
-    },
-  },
-  {
-    'saecki/crates.nvim',
-    event = { 'BufRead Cargo.toml' },
-    opts = {
-      src = {
-        cmp = { enabled = true },
-      },
-    },
-    config = function()
-      require('crates').setup {
-        null_ls = {
-          enabled = true,
-          name = 'crates.nvim',
-        },
-        popup = {
-          border = 'rounded',
-        },
-      }
-    end,
-  },
-  {
-    'simrat39/rust-tools.nvim',
-    lazy = true,
-    opts = function()
-      local ok, mason_registry = pcall(require, 'mason-registry')
-      local adapter ---@type any
-      if ok then
-        -- rust tools configuration for debugging support
-        local codelldb = mason_registry.get_package 'codelldb'
-        local extension_path = codelldb:get_install_path() .. '/extension/'
-        local codelldb_path = extension_path .. 'adapter/codelldb'
-        local liblldb_path = ''
-        if vim.loop.os_uname().sysname:find 'Windows' then
-          liblldb_path = extension_path .. 'lldb\\bin\\liblldb.dll'
-        elseif vim.fn.has 'mac' == 1 then
-          liblldb_path = extension_path .. 'lldb/lib/liblldb.dylib'
-        else
-          liblldb_path = extension_path .. 'lldb/lib/liblldb.so'
+        config = function()
+            local on_attach = function(_, bufnr)
+            end
+            require("phpactor").setup({
+                install = {
+                    bin = vim.fn.stdpath("data") .. "~/.local/share/nvim/mason/bin/phpactor",
+                    php_bin = "php",
+                },
+                lspconfig = {
+                    enabled = true,
+                    options = {
+                        ["language_server_phpstan.enabled"] = true,
+                        ["language_server_phpstan.bin"] = "~/.local/share/nvim/mason/bin/phpstan",
+                        ["language_server_psalm.enabled"] = false,
+                        ["language_server_php_cs_fixer.enabled"] = false,
+                        ["symfony.enabled"] = true,
+                    },
+                }
+            })
         end
-        adapter = require('rust-tools.dap').get_codelldb_adapter(codelldb_path, liblldb_path)
-      end
-      return {
-        dap = {
-          adapter = adapter,
+    },
+    {
+        'Exafunction/codeium.nvim',
+        event = 'BufEnter',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'hrsh7th/nvim-cmp',
+            'onsails/lspkind.nvim',
         },
-        tools = {
-          on_initialized = function()
-            vim.cmd [[
+        config = true,
+    },
+    {
+        'kosayoda/nvim-lightbulb',
+        event = 'LspAttach',
+        config = function()
+            require('nvim-lightbulb').setup {
+                autocmd = { enabled = true },
+                sign = {
+                    enabled = true,
+                    text = require('icons').diagnostics.BoldHint,
+                    hl = 'LightBulbSign',
+                },
+            }
+        end,
+    },
+    {
+        'nvimtools/none-ls.nvim',
+        event = 'VeryLazy',
+        requires = { 'nvim-lua/plenary.nvim' },
+    },
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            { 'williamboman/mason.nvim', config = true },
+            'williamboman/mason-lspconfig.nvim',
+            'folke/neodev.nvim',
+        },
+    },
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lua',
+            'rafamadriz/friendly-snippets',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-nvim-lsp-signature-help',
+            'hrsh7th/cmp-vsnip',
+        },
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        },
+        build = ':TSUpdate',
+    },
+    {
+        'gbprod/php-enhanced-treesitter.nvim',
+        dependencies = {
+            { 'derekstride/tree-sitter-sql', build = ':TSInstall sql' },
+        },
+    },
+    {
+        'saecki/crates.nvim',
+        event = { 'BufRead Cargo.toml' },
+        opts = {
+            src = {
+                cmp = { enabled = true },
+            },
+        },
+        config = function()
+            require('crates').setup {
+                null_ls = {
+                    enabled = true,
+                    name = 'crates.nvim',
+                },
+                popup = {
+                    border = 'rounded',
+                },
+            }
+        end,
+    },
+    {
+        'simrat39/rust-tools.nvim',
+        lazy = true,
+        opts = function()
+            local ok, mason_registry = pcall(require, 'mason-registry')
+            local adapter ---@type any
+            if ok then
+                -- rust tools configuration for debugging support
+                local codelldb = mason_registry.get_package 'codelldb'
+                local extension_path = codelldb:get_install_path() .. '/extension/'
+                local codelldb_path = extension_path .. 'adapter/codelldb'
+                local liblldb_path = ''
+                if vim.loop.os_uname().sysname:find 'Windows' then
+                    liblldb_path = extension_path .. 'lldb\\bin\\liblldb.dll'
+                elseif vim.fn.has 'mac' == 1 then
+                    liblldb_path = extension_path .. 'lldb/lib/liblldb.dylib'
+                else
+                    liblldb_path = extension_path .. 'lldb/lib/liblldb.so'
+                end
+                adapter = require('rust-tools.dap').get_codelldb_adapter(codelldb_path, liblldb_path)
+            end
+            return {
+                dap = {
+                    adapter = adapter,
+                },
+                tools = {
+                    on_initialized = function()
+                        vim.cmd [[
                 augroup RustLSP
                   autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
                   autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
                   autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
                 augroup END
               ]]
-          end,
-        },
-      }
-    end,
-    config = function() end,
-  },
-  {
-    'ray-x/lsp_signature.nvim',
-    event = 'BufRead',
-    config = function()
-      require('lsp_signature').setup()
-    end,
-  },
+                    end,
+                },
+            }
+        end,
+        config = function() end,
+    },
+    {
+        'ray-x/lsp_signature.nvim',
+        event = 'BufRead',
+        config = function()
+            require('lsp_signature').setup()
+        end,
+    },
 }
