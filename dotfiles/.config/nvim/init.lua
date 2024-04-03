ea90946313823d02216f7049fe188e5a0ee49ca7
raul-bns <raul.bethencourt@bluenot-systems.com>
2024-04-03 20:36:57 +0200

    [config] last config nvim

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -1,5 +1,4 @@
---[[
-=====================================================================
+--[[=================================================================
 =====================================================================
 ========                                    .-----.          ========
 ========         .----------------------.   | === |          ========
@@ -16,8 +15,7 @@
 ========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
 ========                                                     ========
 =====================================================================
-=====================================================================
---]]
+=================================================================--]]
 
 -- NOTE: need to set leader and termguicolors before lazy
 vim.g.mapleader = ' '
@@ -27,45 +25,46 @@ vim.opt.termguicolors = true
 -- NOTE: install lazy
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  }
+    vim.fn.system {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable',
+        lazypath,
+    }
 end
+---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 -- NOTE: vscode nvim settings
 if vim.g.vscode then
-  require 'vscode.mappings_vscode'
+    require 'vscode.mappings_vscode'
 
-  local cmd = {
-    [[source $HOME/.config/nvim/vscode/vim_settings.vim]],
-    [[source $HOME/.config/nvim/vscode/functions.vim]],
-    [[source $HOME/.config/nvim/vscode/settings.vim]],
-    [[nnoremap z= <Cmd>call VSCodeNotify('keyboard-quickfix.openQuickFix')<CR>]],
-  }
+    local cmd = {
+        [[source $HOME/.config/nvim/vscode/vim_settings.vim]],
+        [[source $HOME/.config/nvim/vscode/functions.vim]],
+        [[source $HOME/.config/nvim/vscode/settings.vim]],
+        [[nnoremap z= <Cmd>call VSCodeNotify('keyboard-quickfix.openQuickFix')<CR>]],
+    }
 
-  ---@diagnostic disable-next-line: unused-local
-  for i, v in ipairs(cmd) do
-    vim.cmd(v)
-  end
+    ---@diagnostic disable-next-line: unused-local
+    for i, v in ipairs(cmd) do
+        vim.cmd(v)
+    end
 
-  -- NOTE: load vscode plugins
-  require('lazy').setup({
-    { import = 'vscode.plugins' },
-  }, {})
+    -- NOTE: load vscode plugins
+    require('lazy').setup({
+        { import = 'vscode.plugins' },
+    }, {})
 
-  require 'vscode.configs'
+    require 'vscode.configs'
 else
-  -- NOTE: load plugins
-  require('lazy').setup({
-    { import = 'raBeta.plugins' },
-  }, {})
+    -- NOTE: load plugins
+    require('lazy').setup({
+        { import = 'raBeta.plugins' },
+    }, {})
 
-  -- NOTE: load configs
-  require 'raBeta.configs'
+    -- NOTE: load configs
+    require 'raBeta.configs'
 end
--- dotfiles/.config/nvim/lua/raBeta/configs/autocommands.lua
+++ dotfiles/.config/nvim/lua/raBeta/configs/autocommands.lua
@@ -1,42 +1,43 @@
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
-  pattern = { '*' },
-  -- hidde lualine
-  command = 'set ls=0',
+    pattern = { '*' },
+    -- hidde lualine
+    command = 'set ls=0',
 })
 
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
-  pattern = { '*' },
-  -- enable wrap mode for json files only
-  command = 'set fcs=eob:\\ ',
+    pattern = { '*' },
+    -- hidde vim tabline
+    command = 'set showtabline=0 ',
+})
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
+    pattern = { '*' },
+    -- enable wrap mode for json files only
+    command = 'set fcs=eob:\\ ',
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'zsh',
-  callback = function()
-    -- let treesitter use bash highlight for zsh files as well
-    require('nvim-treesitter.highlight').attach(0, 'bash')
-  end,
+    pattern = 'zsh',
+    callback = function()
+        -- let treesitter use bash highlight for zsh files as well
+        require('nvim-treesitter.highlight').attach(0, 'bash')
+    end,
 })
 
-vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
-  pattern = { '*' },
-  -- hidde vim tabline
-  command = 'set showtabline=0 ',
-})
-
-vim.api.nvim_create_autocmd({ 'FileType' }, {
-  pattern = 'sql,mysql,plsql',
-  callback = function()
-    require('cmp').setup.buffer { sources = { { name = 'vim-dadbod-completion' } } }
-  end,
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'hbs',
+    callback = function()
+        -- let treesitter use bash highlight for zsh files as well
+        require('nvim-treesitter.highlight').attach(0, 'javascript')
+    end,
 })
 
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
 })
