66741322e319ef7015ca97839d2681475bb2cdb8
raul-bns <raul.bethencourt@bluenot-systems.com>
2024-04-09 17:18:01 +0200

    [nf] add kemap to change xdebug path in php.lua

--- dotfiles/.config/nvim/lua/raBeta/configs/lsp/languages/php.lua
+++ dotfiles/.config/nvim/lua/raBeta/configs/lsp/languages/php.lua
@@ -7,18 +7,13 @@ dap.adapters.php = {
     args = { mason_path .. 'packages/php-debug-adapter/extension/out/phpDebug.js' },
 }
 
--- local path = '/var/www/html/'
-local path = '/shared/httpd/portal/www/'
-
 dap.configurations.php = {
     {
         name = 'Listen for Xdebug',
         type = 'php',
         request = 'launch',
         port = 9000,
-        pathMappings = {
-            [path] = "${workspaceFolder}",
-        },
+        pathMappings = { ['/var/www/html/'] = "${workspaceFolder}" },
     },
     {
         name = 'Debug currently open script',
@@ -28,8 +23,21 @@ dap.configurations.php = {
         cwd = '${fileDirname}',
         program = '${file}',
         runtimeExecutable = 'php',
-        pathMappings = {
-            [path] = "${workspaceFolder}",
-        },
+        pathMappings = { ['/var/www/html/'] = "${workspaceFolder}" },
     },
 }
+
+-- NOTE: Set Xdebug path with keybinding
+vim.keymap.set('n', '<leader>dp', function()
+    local path = (dap.configurations.php[1].pathMappings['/var/www/html/'] ~= nil and
+        { ['/shared/httpd/portal/www/'] = "${workspaceFolder}" } or
+        { ['/var/www/html/'] = "${workspaceFolder}" })
+
+    ---@diagnostic disable-next-line: inject-field
+    dap.configurations.php[2].pathMappings = path
+    ---@diagnostic disable-next-line: inject-field
+    dap.configurations.php[1].pathMappings = path
+
+    print('The workspaceFolder path is: ' .. string.gsub(vim.inspect(path), '\n', ' '))
+end
+, { noremap = true, silent = true, desc = 'Set Xdebug [P]ath' })
