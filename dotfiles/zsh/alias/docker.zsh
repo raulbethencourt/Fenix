5204d80a75c2334c4f30a2ba74fff3a6f940156a
raul-bns <raul.bethencourt@bluenot-systems.com>
2024-09-04 15:40:53 +0200

    config(zsh): add bash autocomplition and remove some zap plugins

--- dotfiles/zsh/alias/docker.zsh
+++ dotfiles/zsh/alias/docker.zsh
@@ -1,5 +1,5 @@
-#!/bin/sh
-dccmd='docker compose'
+# support Compose v2 as docker CLI plugin
+(( ${+commands[docker-compose]} )) && dccmd='docker-compose' || dccmd='docker compose'
 
 alias dco="$dccmd"
 alias dcb="$dccmd build"
