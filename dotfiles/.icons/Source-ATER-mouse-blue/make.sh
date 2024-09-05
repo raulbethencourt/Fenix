e5f7096ce584d63eeb3005d1ca633b43896b983e
raul-bns <raul.bethencourt@bluenot-systems.com>
2024-09-05 07:58:52 +0200

    config(tweaks): move directories to dotfiles and add gruvbox

---
+++ dotfiles/.icons/Source-ATER-mouse-blue/make.sh
@@ -0,0 +1,201 @@
+#!/bin/sh
+echo left_ptr_watch.in cursors/left_ptr_watch
+xcursorgen left_ptr_watch.in cursors/left_ptr_watch
+
+echo hand1.in cursors/hand1
+xcursorgen hand1.in cursors/hand1
+
+echo hand1.in cursors/hand2
+xcursorgen hand1.in cursors/hand2
+
+echo left_ptr.in cursors/left_ptr
+xcursorgen left_ptr.in cursors/left_ptr
+
+echo center_ptr.in cursors/center_ptr
+xcursorgen center_ptr.in cursors/center_ptr
+
+echo xterm.in cursors/xterm
+xcursorgen xterm.in cursors/xterm
+
+echo crossed_circle.in cursors/crossed_circle
+xcursorgen crossed_circle.in cursors/crossed_circle
+
+echo right_ptr.in cursors/right_ptr
+xcursorgen right_ptr.in cursors/right_ptr
+
+echo copy.in cursors/copy
+xcursorgen copy.in cursors/copy
+
+echo move.in cursors/move
+xcursorgen move.in cursors/move
+
+echo link.in cursors/link
+xcursorgen link.in cursors/link
+
+echo circle.in cursors/circle
+xcursorgen circle.in cursors/circle
+
+echo sb_h_double_arrow.in cursors/sb_h_double_arrow
+xcursorgen sb_h_double_arrow.in cursors/sb_h_double_arrow
+
+echo sb_v_double_arrow.in cursors/sb_v_double_arrow
+xcursorgen sb_v_double_arrow.in cursors/sb_v_double_arrow
+
+echo top_left_corner.in cursors/top_left_corner
+xcursorgen top_left_corner.in cursors/top_left_corner
+
+echo top_right_corner.in cursors/top_right_corner
+xcursorgen top_right_corner.in cursors/top_right_corner
+
+echo bottom_left_corner.in cursors/bottom_left_corner
+xcursorgen bottom_left_corner.in cursors/bottom_left_corner
+
+echo bottom_right_corner.in cursors/bottom_right_corner
+xcursorgen bottom_right_corner.in cursors/bottom_right_corner
+
+echo fd_double_arrow.in cursors/fd_double_arrow
+xcursorgen fd_double_arrow.in cursors/fd_double_arrow
+
+echo bd_double_arrow.in cursors/bd_double_arrow
+xcursorgen bd_double_arrow.in cursors/bd_double_arrow
+
+echo watch.in cursors/watch
+xcursorgen watch.in cursors/watch
+
+echo sb_left_arrow.in cursors/sb_left_arrow
+xcursorgen sb_left_arrow.in cursors/sb_left_arrow
+
+echo sb_right_arrow.in cursors/sb_right_arrow
+xcursorgen sb_right_arrow.in cursors/sb_right_arrow
+
+echo sb_up_arrow.in cursors/sb_up_arrow
+xcursorgen sb_up_arrow.in cursors/sb_up_arrow
+
+echo sb_down_arrow.in cursors/sb_down_arrow
+xcursorgen sb_down_arrow.in cursors/sb_down_arrow
+
+echo based_arrow_down.in cursors/based_arrow_down
+xcursorgen based_arrow_down.in cursors/based_arrow_down
+
+echo based_arrow_up.in cursors/based_arrow_up
+xcursorgen based_arrow_up.in cursors/based_arrow_up
+
+echo bottom_side.in cursors/bottom_side
+xcursorgen bottom_side.in cursors/bottom_side
+
+echo top_side.in cursors/top_side
+xcursorgen top_side.in cursors/top_side
+
+echo left_side.in cursors/left_side
+xcursorgen left_side.in cursors/left_side
+
+echo right_side.in cursors/right_side
+xcursorgen right_side.in cursors/right_side
+
+echo fleur.in cursors/fleur
+xcursorgen fleur.in cursors/fleur
+
+echo question_arrow.in cursors/question_arrow
+xcursorgen question_arrow.in cursors/question_arrow
+
+echo top_tee.in cursors/top_tee
+xcursorgen top_tee.in cursors/top_tee
+
+echo bottom_tee.in cursors/bottom_tee
+xcursorgen bottom_tee.in cursors/bottom_tee
+
+echo left_tee.in cursors/left_tee
+xcursorgen left_tee.in cursors/left_tee
+
+echo right_tee.in cursors/right_tee
+xcursorgen right_tee.in cursors/right_tee
+
+echo ul_angle.in cursors/ul_angle
+xcursorgen ul_angle.in cursors/ul_angle
+
+echo ll_angle.in cursors/ll_angle
+xcursorgen ll_angle.in cursors/ll_angle
+
+echo ur_angle.in cursors/ur_angle
+xcursorgen ur_angle.in cursors/ur_angle
+
+echo lr_angle.in cursors/lr_angle
+xcursorgen lr_angle.in cursors/lr_angle
+
+echo X_cursor.in cursors/X_cursor
+xcursorgen X_cursor.in cursors/X_cursor
+
+echo crosshair.in cursors/crosshair
+xcursorgen crosshair.in cursors/crosshair
+
+echo cross.in cursors/cross
+xcursorgen cross.in cursors/cross
+
+echo tcross.in cursors/tcross
+xcursorgen tcross.in cursors/tcross
+
+echo dotbox.in cursors/dotbox
+xcursorgen dotbox.in cursors/dotbox
+
+echo plus.in cursors/plus
+xcursorgen plus.in cursors/plus
+
+echo pencil.in cursors/pencil
+xcursorgen pencil.in cursors/pencil
+
+echo dnd-none.in cursors/dnd-none
+xcursorgen dnd-none.in cursors/dnd-none
+
+echo dnd-copy.in cursors/dnd-copy
+xcursorgen dnd-copy.in cursors/dnd-copy
+
+echo dnd-link.in cursors/dnd-link
+xcursorgen dnd-link.in cursors/dnd-link
+
+echo dnd-move.in cursors/dnd-move
+xcursorgen dnd-move.in cursors/dnd-move
+
+echo dnd-ask.in cursors/dnd-ask
+xcursorgen dnd-ask.in cursors/dnd-ask
+
+cd cursors
+cp    dotbox			draped_box
+cp    dotbox			icon
+cp    dotbox			target
+cp    dotbox			dot_box_mask
+cp    X_cursor			pirate
+cp    left_ptr_watch	08e8e1c95fe2fc01f976f1e063a24ccd
+cp    left_ptr_watch	3ecb610c1bf2410f44200f48c40d3599
+cp    left_ptr			arrow
+cp    left_ptr			top_left_arrow
+cp    right_ptr			draft_large
+cp    right_ptr			draft_small
+cp    move				4498f0e0c1937ffe01fd06f973665830
+cp    move				9081237383d90e509aa00f00170e968f
+cp    copy				1081e37283d90000800003c07f3ef6bf
+cp    copy				6407b0e94181790501fd1e167b474872
+cp    cross				cross_reverse
+cp    cross				diamond_cross
+cp    hand1				9d800788f1b08800ae810202380a0822
+cp    hand2				e29285e634086352946a0e7090d73106
+cp    hand1				hand
+cp    question_arrow	d9ce0ab605698f320427677b458ad60b
+cp    question_arrow	5c6cd98b3f3ebcb1f9c7f1c204630408
+cp    question_arrow	help
+cp    question_arrow	left_ptr_help
+cp    link				3085a0e285430894940527032f8b26df
+cp    link				640fb0e74195791501fd1ed57b41487f
+cp    crossed_circle	03b6e0fcb3499374a867c041f52298f0
+cp    fd_double_arrow	fcf1c3c7cd4491d801f1e1c78f100000
+cp    bd_double_arrow	c7088f0f3e6c8088236ef8e1e3e70000
+cp    based_arrow_up	base_arrow_up
+cp    based_arrow_down	base_arrow_down
+cp    sb_h_double_arrow h_double_arrow
+cp    sb_h_double_arrow	14fef782d02440884392942c11205230
+cp    h_double_arrow	028006030e0e7ebffc7f7070c0600140
+cp    sb_v_double_arrow double_arrow
+cp    sb_v_double_arrow v_double_arrow
+cp    sb_v_double_arrow	2870a09082c103050810ffdffffe0204
+cp    v_double_arrow	00008160000006810000408080010102
+cp    center_ptr		centre_ptr
+
--- tweaks/.themes/Arquetype-Cinnamon/cinnamon/parse-sass.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-SRC="./sass/cinnamon.scss"
-COMPILE="cinnamon.css"
-
-bundle exec sass --watch $SRC:$COMPILE --sourcemap=none --no-cache --style expanded
