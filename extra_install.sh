8f017f71876134f92599fcc43d9ce0312407a819
raul-bns <raul.bethencourt@bluenot-systems.com>
2024-09-06 09:20:57 +0200

    fix(install): move some packages to nix

--- extra_install.sh
+++ extra_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 nvm install stable
 
 # Recover config from Fenix repo
@@ -9,32 +9,6 @@ git clone https://github.com/raulbethencourt/Fenix.git
 cp -fr "$HOME"/Fenix/dotfiles/* "$HOME"/
 cp -fr "$HOME"/Fenix/dotfiles/.config/* "$HOME"/.config/
 
-# Dragon
-git clone https://github.com/mwh/dragon.git "$HOME"/git_apps/dragon
-cd "$HOME"/git_apps/dragon
-make install
-cd
-
-# sxiv
-git clone https://github.com/xyb3rt/sxiv.git "$HOME"/git_apps/sxiv
-cd "$HOME"/git_apps/sxiv
-make install
-cd
-
-# eza
-cargo install eza
-
-## TODO: add nix packages
-sh <(curl -L https://nixos.org/nix/install) --daemon
-nix-channel --add https://nixos.org/channels/nixpkgs-unstable unstable
-###### tmux
-nix-env -iA unstable.tmux
-###### fzf
-nix-env -iA unstable.fzf
-###### dbeaver
-nix-env -iA unstable.dbeaver-bin
-
 # MANUAL INSTALLATION
-## neovim 
 ## docker
 ## devilbox
