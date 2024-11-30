671b8975cfa4fd0c5e4aca105be936e15c4315c8
raulbethencourt <raul.bethencourt.pro@gmail.com>
2024-11-30 22:31:01 +0100

    refact(install): add while loops for installing packages

--- nix_install.sh
+++ nix_install.sh
@@ -4,32 +4,24 @@
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable unstable
 nix-channel --update
 
-###### tmux
-nix-env -iA unstable.tmux
-###### fzf
-nix-env -iA unstable.fzf
-###### dbeaver
-nix-env -iA unstable.dbeaver-bin
-###### neovim 
-nix-env -iA unstable.neovim
-###### eza 
-nix-env -iA unstable.eza
-###### delta 
-nix-env -iA unstable.delta
-###### bat 
-nix-env -iA unstable.bat
-###### xdragon 
-nix-env -iA unstable.xdragon
-###### sxiv 
-nix-env -iA unstable.sxiv
-###### ccze 
-nix-env -iA unstable.ccze
-###### ripgrep 
-nix-env -iA unstable.ripgrep
-###### fd 
-nix-env -iA unstable.fd
-###### dust 
-nix-env -iA unstable.dust
+while read -r p; do nix-env -iA unstable.$p; done < <(
+  cat <<"EOF"
+  tmux
+  fzf
+  dbeaver-bin
+  neovim
+  eza
+  delta
+  bat
+  xdragon
+  sxiv
+  ccze
+  ripgrep
+  fd
+  dust
+  lynx
+EOF
+)
 
 # bat
 mkdir -p ~/.local/bin
