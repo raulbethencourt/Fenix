75d7737d80c6df07427424465c5a05566c0afb8d
raul-bns <raul.bethencourt@bluenot-systems.com>
2025-01-31 10:15:59 +0100

    fix(zsh): new keymaps and change aliases

--- dotfiles/zsh/alias/general.zsh
+++ dotfiles/zsh/alias/general.zsh
@@ -1,11 +1,11 @@
 alias docker-compose="docker compose"
 alias c="clear"
-alias po="ascii-image-converter ~/Images/wallpapers/gadgetman.png -C && sleep 2 && poweroff"
-alias rb="ascii-image-converter ~/Images/wallpapers/gadgetman.png -C && sleep 2 && reboot"
+alias po="ascii-image-converter ~/Images/wallpapers/crazy_dev.png && sleep 2 && poweroff"
+alias rb="ascii-image-converter ~/Images/wallpapers/crazy_dev.png && sleep 2 && reboot"
 alias l="exa -laa --icons  --color-scale"
 alias lt="exa -laT --icons  --color-scale"
 alias sudo="sudo "
 alias cdf=". cdf"
 alias tldr="tldr -t ocean"
 alias vim='nvim'
-alias grepc='grep --color=always'
+alias grep='grep --color=auto'
