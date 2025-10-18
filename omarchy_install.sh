72abbe7036a26f9564b1938d08b21e61b7f0e258
raulbethencourt <raul.bethencourt.pro@gmail.com>
2025-10-18 22:53:33 +0200

    feat(omarchy): add new config

---
+++ omarchy_install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -eu -o pipefail # fail on error and report it, debug all lines
+
+while read -r p; do sudo pacman --noconfirm -S "$p"; done < <(
+  cat <<"EOF"
+  brave-browser
+  cmake
+  dust
+  figlet
+  git-delta
+  jq
+  lynx
+  sxiv
+  tmux
+  yt-dlp
+  zathura
+  zsh
+  tree
+  transmission-gtk
+  vlc
+  vlc-plugin-ffmpeg
+  vlc-plugin-x264
+  vlc-plugin-x265
+  vlc-cli
+EOF
+)
+
+# make zsh shell
+chsh -s $(which zsh)
+
+# zap !!!
+zsh <(curl -s https:/raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+
+# Omarchy manual development installation
+#  php
+#  go
+#  rust
+
+# Omarchy manual AUR installation
+#  ccze
+#  clipse
