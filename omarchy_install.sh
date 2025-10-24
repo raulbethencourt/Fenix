23f2e2e411f5781d8a18f49b619cb8fdb4386fbe
raulbethencourt <raul.bethencourt.pro@gmail.com>
2025-10-25 00:28:29 +0200

    fix(omarchy_install): add clone repos and cp config

--- omarchy_install.sh
+++ omarchy_install.sh
@@ -33,7 +33,32 @@ chsh -s $(which zsh)
 # zap !!!
 zsh <(curl -s https:/raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
 
+# Nicest omarchy theme
 omarchy-theme-install https://github.com/JaxonWright/omarchy-midnight-theme.git
+
+# Install my nvim config
+mv $HOME/.config/nvim{,.bak}
+gh repo clone raulbethencourt/nvim $HOME/.config/nvim_test
+
+# Tools
+gh repo clone raulbethencourt/tools $HOME/tools
+
+# Vaults
+gh repo clone raulbethencourt/vaults $HOME/vaults
+
+[ ! -d $HOME/Fenix ] && echo "Fenix directory not found it in proper location." && exit 1
+
+# Install rest of my config
+cp -rf $HOME/Fenix/dotfiles/zsh $HOME/zsh
+cp -f $HOME/Fenix/dotfiles/.zshrc $HOME/.zshrc
+cp -f $HOME/Fenix/dotfiles/.tmux.conf $HOME/.tmux.conf
+cp -f $HOME/Fenix/dotfiles/lynx.lss $HOME/lynx.lss
+cp -f $HOME/Fenix/dotfiles/.lynxrc $HOME/.lynxrc
+cp -f $HOME/Fenix/dotfiles/.gitconfig $HOME/.gitconfig
+cp -f $HOME/Fenix/dotfiles/.bashrc $HOME/.bashrc
+cp -rf $HOME/Fenix/dotfiles/.config/. $HOME/.config/
+cp -rf $HOME/Fenix/dotfiles/devilbox $HOME/devilbox
+cp -rf $HOME/Fenix/dotfiles/.fonts/* $HOME/.local/share/fonts/
 
 # Omarchy manual development installation
 #  php
